e8c124362aed18a8e5d6c144ad87d0cfb2eafd83
MarenSusanne <Marensut@getacademy.no>
2024-05-16 09:45:01 +0200

    Add project files.

---
+++ HvaGjørDenneKoden/Program.cs
@@ -0,0 +1,24 @@
+﻿var range = 250;
+var counts = new int[range];
+var totalcount = 0;
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    foreach (var character in text.ToLower() ?? string.Empty)
+    {
+        totalcount++;
+        counts[character]++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            var percentage = 100 * counts[i] / totalcount ;
+            string answer = character + " - " + percentage + "%";
+            Console.CursorLeft = 20;//Console.BufferWidth - answer.Length - 1;
+            Console.WriteLine(answer);
+        }
+    }
+}
